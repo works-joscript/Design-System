aa587c9149e7c35e441bdf58e7895dbfc3281986
ashraf <ashrafmohamed1176@gmail.com>
2023-10-15 22:03:40 +0300

    finsh home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>eiruodv</h1>
-
+      <Home />
     </div>
   );
 }
